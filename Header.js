8546201c9bad64466c1ad144fc85ba875e80065d
Padmanabh Kharat <70370670+Padmanabh82@users.noreply.github.com>
2021-10-20 16:44:11 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title,onAdd,showAdd}) => {
+
+    function onOff() {
+        var ww = window.open(window.location, '_self'); ww.close();
+      }
+
+    return (
+        <header className='header'>
+            <h1>
+                {title}
+            </h1>
+            <Button color={showAdd ?'red':'green'} text={showAdd ? 'Close' :'Add'} onClick={onAdd}/>
+            <Button color={'red'} text={'Cancel'} onClick={onOff}/>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title:'Test Task Tracker',
+}
+
+Header.propTypes={
+    title:PropTypes.string.isRequired,
+}
+
+export default Header
