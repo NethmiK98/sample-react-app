6c385ea2efb33bd4ec78870bc71ec53ccf62619e
Avishka Sooriyapperuma <71138570+AvishkaSooriyapperuma@users.noreply.github.com>
2021-10-26 14:11:07 +0530

    New cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Navbar from "./App/Navbar/Navbar";
 import Info from "./App/Info/Info";
-
+import Card from "./App/Cards/card"
 function App() {
   return (
     
       <div className="App">
         <Navbar/>
+        <Card/>
         <Info/>
+       
       </div>)
     
   }
